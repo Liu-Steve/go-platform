4ea46b59334c68f4410f85aea8199a687924d2c1
Liu-Steve <steve_liu@whu.edu.cn>
2023-10-13 06:03:15 +0000

    ui更换vue脚手架，server修改

--- script/copy-static.py
+++ script/copy-static.py
@@ -4,10 +4,5 @@ os.chdir(os.path.dirname(__file__))
 print("Working dir:", os.getcwd())
 
 static_dir = "../server/go-server/src/main/resources/static"
-os.system(f"rm -r {static_dir}/css")
-os.system(f"rm -r {static_dir}/js")
-os.system(f"rm -r {static_dir}/img")
-os.system(f"cp ../ui/index.html {static_dir}/index.html")
-os.system(f"cp -r ../ui/css {static_dir}/css")
-os.system(f"cp -r ../ui/js {static_dir}/js")
-os.system(f"cp -r ../ui/img {static_dir}/img")
+os.system(f"rm -r {static_dir}/*")
+os.system(f"cp -r ../ui/dist/* {static_dir}")
